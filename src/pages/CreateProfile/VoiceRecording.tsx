9f7316a1f7d90e04c7f6f3a40b9711786cbe84fc
John Riviello <john_riviello@comcast.com>
2023-07-06 22:05:20 -0400

    Audio recording updates, including timer

--- src/pages/CreateProfile/VoiceRecording.tsx
+++ src/pages/CreateProfile/VoiceRecording.tsx
@@ -1,17 +1,18 @@
 import styles from './profile.module.css';
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import useLocalStorageState from 'use-local-storage-state';
 import { IonPage, IonHeader, IonButton, IonContent,
-  IonToolbar, IonTitle,
+  IonToolbar, IonTitle, IonItem, IonLabel, IonSpinner,
 IonButtons, IonMenuButton } from '@ionic/react';
 import Menu from '../../components/Menu';
 import Footer from '../../components/Footer';
 
-// https://web.dev/patterns/media/microphone-record/
-
 const VoiceRecording: React.FC = () => {
   let [recording, setRecording] = useLocalStorageState('recording', { defaultValue: ''});
-  let mediaRecorder;
+  let [recordingInProgress, setRecordingInProgress] = useState(false);
+  let [recordingTime, setRecordingTime] = useState(0);
+  let timerRef = useRef(null);
+  let mediaRecorder = useRef(null);
 
   function blobToBase64(blob): Promise<string> {
     return new Promise((resolve, _) => {
@@ -21,23 +22,31 @@ const VoiceRecording: React.FC = () => {
     });
   }
 
+  useEffect(() => {
+    if(recordingInProgress){
+      timerRef.current = setTimeout(() => {
+        setRecordingTime(recordingTime + 1);
+      }, 1000);
+    }
+  }, [recordingTime, recordingInProgress]);
 
   const handleSuccess = function(stream:any) {
     const recordedChunks = [];
-    mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm'});
+    setRecordingInProgress(true);
+    mediaRecorder.current = new MediaRecorder(stream, {mimeType: 'audio/webm'});
 
-    mediaRecorder.addEventListener('dataavailable', function(e) {
+    mediaRecorder.current.addEventListener('dataavailable', function(e) {
       if (e.data.size > 0) recordedChunks.push(e.data);
     });
 
-    mediaRecorder.addEventListener('stop', function() {
-      let r = new Blob(recordedChunks)
+    mediaRecorder.current.addEventListener('stop', function() {
+      let r = new Blob(recordedChunks);
       blobToBase64(r).then(result => {
-        setRecording(result)
+        setRecording(result);
       });
     });
 
-    mediaRecorder.start();
+    mediaRecorder.current.start();
   };
 
   function clearRecording() {
@@ -45,14 +54,15 @@ const VoiceRecording: React.FC = () => {
   }
 
   function stopRecording() {
-    mediaRecorder.stop();
+    mediaRecorder.current.stop();
+    setRecordingInProgress(false);
+    clearTimeout(timerRef.current);
   }
 
   function startRecording() {
     navigator.mediaDevices.getUserMedia({ audio: true, video: false })
       .then(handleSuccess);
   }
-
 
   return (
     <IonPage>
@@ -66,16 +76,31 @@ const VoiceRecording: React.FC = () => {
       </IonHeader>
       <Menu></Menu>
       <IonContent class="ion-padding" id="main-content">
+        <h1>Recording Audio 🎙️</h1>
+        <ul>
+          <li><a href="https://web.dev/patterns/media/microphone-record/" rel="noreferrer" target="_blank">web.dev: How to record audio from the user's microphone</a></li>
+          <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio" rel="noreferrer" target="_blank">MDN: &lt;audio&gt;: The Embed Audio element</a></li>
+          <li><a href="https://learnersbucket.com/examples/interview/increment-counter-component-in-react/" rel="noreferrer" target="_blank">Increment counter component in React</a></li>
+        </ul>
+        <p>
+          Record audio from the mic. Display a timer while recording the audio & save it once the recording stops.
+        </p>
+        <p>
+          Once the recording is saved, allow the audio to be played back and also have a button to clear the audio recording.
+        </p>
         <div className={styles.imgContainer}>
-          <audio controls className={styles.audio}>
-            { recording && <source type="audio/wav" src={recording} /> }
-          </audio>
+          { recording &&
+            <audio controls className={styles.audio}>
+              <source type="audio/webm" src={recording} />
+            </audio>
+          }
         </div>
         <div className='ion-text-center'>
+        {recordingInProgress && <IonItem color="danger"><IonSpinner name="circles" color="light"></IonSpinner><IonLabel class="ion-text-center">Recording In Progress - {recordingTime}s</IonLabel><IonSpinner name="circles" color="light"></IonSpinner></IonItem>}
           {recording ?
             <IonButton shape="round" onClick={clearRecording}>Clear Recording</IonButton>
-            : <IonButton shape="round" onClick={startRecording}>Start Recording</IonButton>}
-          <IonButton shape="round" onClick={stopRecording}>Stop Recording</IonButton>
+            : !recordingInProgress && <IonButton shape="round" onClick={startRecording}>Start Recording</IonButton>}
+          {recordingInProgress && <IonButton shape="round" onClick={stopRecording}>Stop Recording</IonButton>}
         </div>
       </IonContent>
       <Footer prev='/profile/upload' next='/profile/notifications'></Footer>
